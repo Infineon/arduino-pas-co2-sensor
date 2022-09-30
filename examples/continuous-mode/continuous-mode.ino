76f72ca8a0a80f32f584b13d431914417b0b2e66
enriquezgarc <enriquezgarcia.external@infineon.com>
2022-09-30 09:36:25 +0200

    Updated delay after serial begin. Removed delay before.

--- examples/continuous-mode/continuous-mode.ino
+++ examples/continuous-mode/continuous-mode.ino
@@ -23,9 +23,8 @@ Error_t err;
 
 void setup()
 {
-    delay(800);
     Serial.begin(9600);
-    delay(500);
+    delay(800);
     Serial.println("serial initialized");
 
     /* Initialize the i2c interface used by the sensor */
