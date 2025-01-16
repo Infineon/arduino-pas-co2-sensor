b8053d6ca7396ef802dc9d7db61a02822803d360
dineshgit411 <Dinesh.M-EE@infineon.com>
2025-01-16 14:02:06 +0530

    Define interrupt pin staus for renesas board architecture

--- src/pas-co2-ino.cpp
+++ src/pas-co2-ino.cpp
@@ -352,7 +352,7 @@ Error_t PASCO2Ino::startMeasure(int16_t periodInSec, int16_t alarmTh, void (*cba
     {
         /* Enable sensor interrupt */
         intConf.b.int_typ = XENSIV_PASCO2_INTERRUPT_TYPE_HIGH_ACTIVE;
-        #ifdef ARDUINO_ARCH_SAMD
+        #if defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_RENESAS)
             PinStatus int_event;
         #else
             uint8_t int_event;
